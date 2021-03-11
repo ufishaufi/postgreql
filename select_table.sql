d93df8f125bdf964e1c40fe457cae06ac19e7c83
Shaufi <shaufiman@gmail.com>
2021-03-11 11:45:47 +0800

    update materi postgresql

--- select_table.sql
+++ select_table.sql
@@ -68,4 +68,7 @@ GROUP BY
 SELECT country_of_birth FROM person GROUP BY country_of_birth;
 SELECT country_of_birth, COUNT(*) FROM person GROUP BY country_of_birth;
 SELECT country_of_birth, COUNT(*) FROM person GROUP BY country_of_birth ORDER BY country_of_birth;
+
+GROUP BY HAVING
+
 SELECT country_of_birth, COUNT(*) FROM person GROUP BY country_of_birth HAVING COUNT(*) > 5 ORDER BY country_of_birth; 
