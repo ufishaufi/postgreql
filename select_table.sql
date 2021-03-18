53680d5f9abe7caf33b248d11b4d1f466cebeb63
Shaufi <shaufiman@gmail.com>
2021-03-18 20:24:38 +0800

    update materi alias and coalesce

--- select_table.sql
+++ select_table.sql
@@ -107,3 +107,7 @@ ARITHMETIC OPERATORS ROUND
 
 SELECT id, make, model, price, ROUND(price * .10, 2) FROM car;
 SELECT id, make, model, price, ROUND(price * .10, 2) ROUND(price - (price * .10) FROM car;
+
+ALIAS
+
+SELECT id, make, model, price AS original_price, ROUND(price * .10, 2) AS ten_percent_value, ROUND(price - (price * .10) AS discount_after_ten_percent  FROM car;
